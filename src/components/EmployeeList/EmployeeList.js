fd5cd18564417c28dbdbd255e4efb9d7cf599d8f
Axel GMX <agmx183@gmail.com>
2024-09-03 09:45:56 +0200

    HRnet app in React

---
+++ src/components/EmployeeList/EmployeeList.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { useTable } from "react-table";
+
+import "./EmployeeList.css";
+
+const EmployeeList = () => {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    const storedEmployees = JSON.parse(localStorage.getItem("employees")) || [];
+    setEmployees(storedEmployees);
+  }, []);
+
+  const data = React.useMemo(() => employees, [employees]);
+
+  const columns = React.useMemo(
+    () => [
+      { Header: "First Name", accessor: "firstName" },
+      { Header: "Last Name", accessor: "lastName" },
+      { Header: "Start Date", accessor: "startDate" },
+      { Header: "Department", accessor: "department" },
+      { Header: "Date of Birth", accessor: "dateOfBirth" },
+      { Header: "Street", accessor: "street" },
+      { Header: "City", accessor: "city" },
+      { Header: "State", accessor: "state" },
+      { Header: "Zip Code", accessor: "zipCode" },
+    ],
+    []
+  );
+
+  const tableInstance = useTable({ columns, data });
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    tableInstance;
+
+  return (
+    <div className="container">
+      <h2>Current Employees</h2>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row) => {
+            prepareRow(row);
+            return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => (
+                  <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default EmployeeList;
