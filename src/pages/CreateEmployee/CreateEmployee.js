da920044ff98ccb11cc81dc40f6b8d1d7b695b1a
Axel GMX <agmx183@gmail.com>
2024-09-06 02:22:52 +0200

    Plugin : Import Modal Component ( npm )

--- src/pages/CreateEmployee/CreateEmployee.js
+++ src/pages/CreateEmployee/CreateEmployee.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import ModalComponent from "../../components/ModalComponent/ModalComponent";
 import EmployeeForm from "./../../components/EmployeeForm/EmployeeForm";
+import ModalComponent from "react-modal-plugin-hrnet-agmx";
 import "./CreateEmployee.css";
 
 /**
@@ -21,7 +21,9 @@ const CreateEmployee = () => {
   return (
     <div className="container ">
       <h1>Create Employee</h1>
-      <Link to={"/employee-list"} className="page-link">View Current Employees</Link>
+      <Link to={"/employee-list"} className="page-link">
+        View Current Employees
+      </Link>
       <EmployeeForm onSuccess={handleSuccess} />
       <ModalComponent isOpen={isModalOpen} onClose={closeModal} />
     </div>
