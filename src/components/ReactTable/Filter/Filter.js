d1c6e32754bff9d5ca531931659ede43f2de3788
Axel GMX <agmx183@gmail.com>
2024-09-11 10:25:39 +0200

    Filter : update

--- src/components/ReactTable/Filter/Filter.js
+++ src/components/ReactTable/Filter/Filter.js
@@ -3,17 +3,24 @@ import "./Filter.css";
 
 const Filter = ({ setPageSize, pageSize }) => {
   return (
-    <select
-      name="filter"
-      value={pageSize}
-      onChange={(e) => setPageSize(Number(e.target.value))}
-    >
-      {[10, 25, 50, 100].map((size) => (
-        <option key={size} value={size}>
-          Show {size} pages
-        </option>
-      ))}
-    </select>
+    <div>
+      <label htmlFor="filter" name="filter">
+        Show
+        <select
+          id="filter"
+          name="filter"
+          value={pageSize}
+          onChange={(e) => setPageSize(Number(e.target.value))}
+        >
+          {[10, 25, 50, 100].map((size) => (
+            <option key={size} value={size}>
+              {size}
+            </option>
+          ))}
+        </select>
+        entries
+      </label>
+    </div>
   );
 };
 
